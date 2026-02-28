370fc9d864c6a056efdf5e877ab754a42c396310
bubu <1654388696@qq.com>
2026-02-28 12:17:14 +0800

    feat(api-contract): add api response contract module

---
+++ server/api-contract/src/main/java/com/linkforge/contract/api/ApiResponse.java
@@ -0,0 +1,63 @@
+package com.linkforge.contract.api;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiResponse<T> {
+
+    private int code;
+    private String message;
+    private T data;
+    private String requestId;
+
+    public ApiResponse() {
+    }
+
+    private ApiResponse(int code, String message, T data, String requestId) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.requestId = requestId;
+    }
+
+    public static <T> ApiResponse<T> ok(T data, String requestId) {
+        return new ApiResponse<>(0, "ok", data, requestId);
+    }
+
+    public static <T> ApiResponse<T> error(int code, String message, String requestId) {
+        return new ApiResponse<>(code, message, null, requestId);
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+}
+
